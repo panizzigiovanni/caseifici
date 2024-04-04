4273de8115289c000aea4f54d474bcba6f40a237
EdoardoFalcini <148615454+EdoardoFalcini@users.noreply.github.com>
2024-04-04 10:00:38 +0200

    inizio relazioni foreign key
    svolto le foreign key, mancanti solo una della tabella caseifici e rivedere se completata la tabvella vendite

--- database/caseifici (1).sql
+++ database/caseifici (1).sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 02, 2024 alle 15:26
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
+-- Creato il: Apr 04, 2024 alle 09:58
+-- Versione del server: 10.4.10-MariaDB
+-- Versione PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,7 +37,7 @@ CREATE TABLE `caseifici` (
   `cas_NumTel` varchar(12) NOT NULL,
   `cas_pro_Id` int(3) NOT NULL,
   `cas_ut_Id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -48,7 +49,7 @@ CREATE TABLE `clienti` (
   `cli_Id` int(3) NOT NULL,
   `cli_Nome` varchar(30) NOT NULL,
   `cli_Tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,7 @@ CREATE TABLE `forme` (
   `for_Stagionatura` varchar(8) NOT NULL,
   `for_Scelta` tinyint(1) NOT NULL,
   `for_cas_Id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -76,7 +77,7 @@ CREATE TABLE `fotografie` (
   `fot_Id` int(9) NOT NULL,
   `fot_Percorso` varchar(255) NOT NULL,
   `fot_cas_Id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -92,7 +93,7 @@ CREATE TABLE `giornatelav` (
   `gioLav_QuaPro` int(3) NOT NULL,
   `gioLav_QuaVend` int(4) NOT NULL,
   `gioLav_cas_Id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -104,7 +105,7 @@ CREATE TABLE `province` (
   `pro_Id` int(3) NOT NULL,
   `pro_Sigla` varchar(2) NOT NULL,
   `pro_Regione` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -118,7 +119,7 @@ CREATE TABLE `utenti` (
   `ut_password` varchar(255) NOT NULL,
   `ut_nome` varchar(20) NOT NULL,
   `ut_cognome` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -131,7 +132,7 @@ CREATE TABLE `vendite` (
   `ven_Data` date NOT NULL,
   `ven_for_Id` int(10) NOT NULL,
   `ven_cli_Id` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indici per le tabelle scaricate
@@ -141,7 +142,8 @@ CREATE TABLE `vendite` (
 -- Indici per le tabelle `caseifici`
 --
 ALTER TABLE `caseifici`
-  ADD PRIMARY KEY (`cas_Id`);
+  ADD PRIMARY KEY (`cas_Id`),
+  ADD KEY `fk_cas_pro_Id` (`cas_pro_Id`);
 
 --
 -- Indici per le tabelle `clienti`
@@ -153,19 +155,22 @@ ALTER TABLE `clienti`
 -- Indici per le tabelle `forme`
 --
 ALTER TABLE `forme`
-  ADD PRIMARY KEY (`for_Id`);
+  ADD PRIMARY KEY (`for_Id`),
+  ADD KEY `fk_for_cas_Id` (`for_cas_Id`);
 
 --
 -- Indici per le tabelle `fotografie`
 --
 ALTER TABLE `fotografie`
-  ADD PRIMARY KEY (`fot_Id`);
+  ADD PRIMARY KEY (`fot_Id`),
+  ADD KEY `fk_fot_cas_Id` (`fot_cas_Id`);
 
 --
 -- Indici per le tabelle `giornatelav`
 --
 ALTER TABLE `giornatelav`
-  ADD PRIMARY KEY (`gioLav_Id`);
+  ADD PRIMARY KEY (`gioLav_Id`),
+  ADD KEY `fk_gioLav_cas_Id` (`gioLav_cas_Id`);
 
 --
 -- Indici per le tabelle `province`
@@ -183,7 +188,9 @@ ALTER TABLE `utenti`
 -- Indici per le tabelle `vendite`
 --
 ALTER TABLE `vendite`
-  ADD PRIMARY KEY (`ven_Id`);
+  ADD PRIMARY KEY (`ven_Id`),
+  ADD KEY `fk_ven_cli_Id` (`ven_cli_Id`),
+  ADD KEY `fk_ven_for_Id` (`ven_for_Id`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
@@ -236,6 +243,41 @@ ALTER TABLE `utenti`
 --
 ALTER TABLE `vendite`
   MODIFY `ven_Id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `caseifici`
+--
+ALTER TABLE `caseifici`
+  ADD CONSTRAINT `fk_cas_pro_Id` FOREIGN KEY (`cas_pro_Id`) REFERENCES `province` (`pro_Id`);
+
+--
+-- Limiti per la tabella `forme`
+--
+ALTER TABLE `forme`
+  ADD CONSTRAINT `fk_for_cas_Id` FOREIGN KEY (`for_cas_Id`) REFERENCES `caseifici` (`cas_Id`);
+
+--
+-- Limiti per la tabella `fotografie`
+--
+ALTER TABLE `fotografie`
+  ADD CONSTRAINT `fk_fot_cas_Id` FOREIGN KEY (`fot_cas_Id`) REFERENCES `caseifici` (`cas_Id`);
+
+--
+-- Limiti per la tabella `giornatelav`
+--
+ALTER TABLE `giornatelav`
+  ADD CONSTRAINT `fk_gioLav_cas_Id` FOREIGN KEY (`gioLav_cas_Id`) REFERENCES `caseifici` (`cas_Id`);
+
+--
+-- Limiti per la tabella `vendite`
+--
+ALTER TABLE `vendite`
+  ADD CONSTRAINT `fk_ven_cli_Id` FOREIGN KEY (`ven_cli_Id`) REFERENCES `clienti` (`cli_Id`),
+  ADD CONSTRAINT `fk_ven_for_Id` FOREIGN KEY (`ven_for_Id`) REFERENCES `forme` (`for_Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
